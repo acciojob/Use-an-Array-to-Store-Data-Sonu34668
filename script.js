06b238d4f6a3aafbfe317e1a827cb7dfde360c8f
Sonu34668 <130197661+Sonu34668@users.noreply.github.com>
2023-06-09 13:14:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const yourArray = ['one', 2, 'three', true, false, undefined, null];
+let yourArray = ['sonu', 2, 'three', true, false, undefined, null];
 console.log(yourArray.length);
-console.log(yourArray);
+
